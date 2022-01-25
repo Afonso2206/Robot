13760382994d7c4a9d22c9dc74a8ff5aef7c04f4
Jorn Teurlings <44932251+Jornolo1@users.noreply.github.com>
2022-01-25 12:03:10 +0000

    Initial Files
    Lidar is operational and the binary occupancy map is functional

---
+++ binaryocc.m
@@ -0,0 +1,53 @@
+
+%% The generation of the map
+
+image = imread('tecnico_grid.png');
+imwrite(image, 'tecnico_grid.pgm');
+imagePGM = imread('tecnico_grid.pgm');
+
+imageBW = imagePGM > 254;
+imageWB = 1 - imageBW;
+
+map = binaryOccupancyMap(imageWB);
+
+%% The initialization of the MCL
+
+mcl = monteCarloLocalization;
+mcl.UseLidarScan = true;
+mcl.GlobalLocalization = true;
+mcl.ResamplingInterval = 1;
+
+%% LidarScan
+lidar = rangeSensor;
+lidar.HorizontalAngle = [-pi/2 pi/2];
+lidar.Range = [0 20];
+vehiclePose = [360 230 0];
+
+[ranges, angles] = lidar(vehiclePose, map);
+
+scan = lidarScan(ranges, angles);
+
+%% odometryMotionModel
+
+odo = odometryMotionModel;
+odo.Noise = [0.2 0.2 0.2 0.2];
+
+%% LikelihoodFieldSensorModel
+
+lf = likelihoodFieldSensorModel;
+lf.SensorLimits = [0.5 20];
+lf.Map = map;
+lf.SensorPose = vehiclePose;
+
+%% Combining all the different models
+
+mcl.SensorModel = lf;
+mcl.MotionModel = odo;
+
+%% Get the final particles with their weights
+
+
+
+
+
+
