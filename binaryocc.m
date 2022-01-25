7e13db61a4e80959abf9d3964c8828a95b4c7526
Jorn Teurlings <jornkay@live.nl>
2022-01-25 20:06:54 +0000

    Added artificial waypoints for the car to see

--- binaryocc.m
+++ binaryocc.m
@@ -1,4 +1,7 @@
 
+
+
+
 %% The generation of the map
 
 image = imread('tecnico_grid.png');
@@ -9,23 +12,20 @@ imageBW = imagePGM > 254;
 imageWB = 1 - imageBW;
 
 map = binaryOccupancyMap(imageWB);
+show(map);
 
 %% The initialization of the MCL
 
 mcl = monteCarloLocalization;
 mcl.UseLidarScan = true;
-mcl.GlobalLocalization = true;
+mcl.InitialPose = [360 230 0];
+mcl.InitialCovariance = eye(3)*0.5;
 mcl.ResamplingInterval = 1;
 
 %% LidarScan
 lidar = rangeSensor;
 lidar.HorizontalAngle = [-pi/2 pi/2];
 lidar.Range = [0 20];
-vehiclePose = [360 230 0];
-
-[ranges, angles] = lidar(vehiclePose, map);
-
-scan = lidarScan(ranges, angles);
 
 %% odometryMotionModel
 
@@ -35,21 +35,47 @@ odo.Noise = [0.2 0.2 0.2 0.2];
 %% LikelihoodFieldSensorModel
 
 lf = likelihoodFieldSensorModel;
-lf.SensorLimits = [0.5 20];
+lf.SensorLimits = [0.5 40];
 lf.Map = map;
-lf.SensorPose = vehiclePose;
 
-%% Combining all the different models
+
+%% Turning on the localization
+vehiclePose = [
+    360 230 0;
+    363 231 15;
+    364 235 60;
+    360 240 110;
+    360 245 60;
+    360 260 90;
+    350 240 270;
+    330 230 -45;
+    331 229 -50;
+    332 227 -30;
+    334 226 -15;
+    337 225 0;
+    340 225 0;
+    345 225 0;
+    ];
+
+lf.SensorPose = vehiclePose(1, :);
 
 mcl.SensorModel = lf;
 mcl.MotionModel = odo;
 
+numUpdates = 15;
+i = 1;
+while i < numUpdates
 
-%% Turning on the localization
+    pose = vehiclePose(i, :);
+    [ranges, angles] = lidar(pose, map);
+    scan = lidarScan(ranges, angles);
 
-[isUpdated, estimatedPose, covariance] = mcl(vehiclePose, scan);
-
-%% Get the final particles with their weights
+    [isUpdated, estimatedPose, estimatedCovariance] = mcl(pose, scan);
+    estimatedPose
+    if isUpdated 
+        i = i + 1;
+    end
+end
 
 
 
