fc82b7893c83b40b5387e9f3171cd39157cd6723
Jorn Teurlings <jornkay@live.nl>
2022-01-27 15:24:26 +0000

    Updated
    Has the custom particle filter in it

--- binaryocc.m
+++ binaryocc.m
@@ -1,5 +1,5 @@
-clear all
-close all
+clear all;
+close all;
 %% The generation of the map
 
 image = imread('tecnico_grid.png');
@@ -17,7 +17,7 @@ mcl = monteCarloLocalization;
 
 %% LidarScan
 lidar = rangeSensor;
-lidar.HorizontalAngle = [-pi/2 pi/2];
+lidar.HorizontalAngle = [-pi pi];
 lidar.Range = [0 30];
 
 %% odometryMotionModel
@@ -30,6 +30,7 @@ odo.Noise = [0.2 0.2 0.2 0.2];
 lf = likelihoodFieldSensorModel;
 lf.SensorLimits = [0 30];
 lf.Map = map;
+lf.SensorPose = [0 0 0];
 
 %% Helper methods
 visualizationHelper = ExampleHelperMCLVisualization(map);
@@ -38,12 +39,12 @@ visualizationHelper = ExampleHelperMCLVisualization(map);
 
 
 mcl.SensorModel = lf;
-mcl.MotionModel = odo;
+%mcl.MotionModel = odo;
 
 mcl.ParticleLimits = [500 5000];
 mcl.GlobalLocalization = false;
-mcl.UseLidarScan = true;
 mcl.InitialCovariance = 0;
+mcl.UseLidarScan = true;
 mcl.ResamplingInterval = 1;
 
 %% Creating the trajectory
@@ -66,11 +67,37 @@ for i=1:positionSize
         current = vehiclePose(i, :);
         next = vehiclePose(i+1, :);
         vehiclePose(i, 3) = atan2(next(2)-current(2),next(1)-current(1));
+    else 
+        vehiclePose(i, 3) = vehiclePose(i-1, 3);
     end
 end
+%% Testing for custom Particle Filter
+
+% policy = resamplingPolicyPF;
+% policy.TriggerMethod = 'interval';
+% policy.SamplingInterval = 1;
+% pf = stateEstimatorPF;
+% pf.StateEstimationMethod = 'mean';
+% pf.ResamplingMethod = 'systematic';
+% pf.ResamplingPolicy = policy;
+% 
+% initialize(pf, 1000, vehiclePose(1, :), eye(3));
+% 
+% robotPred = zeros(positionSize, 3);
+% robotCorrected = zeros(positionSize, 3);
+% numUpdates = length(vehiclePose);
+% i = 1;
+% hold on;
+% while i < numUpdates
+%     [robotPred(i, :), robotCov] = predict(pf);
+%     [robotCorrected(i, :), robotCov] = correct(pf, vehiclePose(i, :));
+%     i = i + 1;
+% end
+% plot(robotCorrected(:,1), robotCorrected(:, 2), 'or');
+%% Position tracking
 
 mcl.InitialPose = vehiclePose(1, :);
-numUpdates = length(vehiclePose)-1;
+numUpdates = length(vehiclePose);
 i = 1;
 hold on;
 while i < numUpdates
